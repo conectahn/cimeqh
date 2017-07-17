6472d33a5d1de34549f1c62cd87c3ca5e0a3ee14
juanklpz <juanfilpz@gmail.com>
2017-07-17 16:23:13 -0600

    prueba 2

--- clases/comentariosDespeje.php
+++ clases/comentariosDespeje.php
@@ -1,7 +1,7 @@
 <?php
 require_once("libs/dao.php");
 require_once('models/despeje.model.php');
-//987987
+//987987987879987
 class vistaComentariosDespeje
 {
   public static function vista($tipo,$codigo,$proyectoNombre,$depto,$direccion,
