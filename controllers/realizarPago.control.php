3c3aab2f825098f005d6ba62672075ef3f8ddee2
JuanK Lopez <juanfilpz@gmail.com>
2017-08-23 00:11:08 -0600

    PAGOS DE STRIPE MAS CERCA DE LA RESPUESTA

--- controllers/realizarPago.control.php
+++ controllers/realizarPago.control.php
@@ -6,33 +6,25 @@
   require 'pagar/lib/Stripe.php';
   function run(){
 
-  $errores = array();
   $htmlDatos = array();
-
-  if ($_POST) {
-        Stripe::setApiKey("sk_test_cg3QY2mbcJvek1EVBopLsavG");
-        try {
-        if (empty($_POST['street']) || empty($_POST['city']) || empty($_POST['zip']))
-            throw new Exception("Fill out all required fields.");
-          if (!isset($_POST['stripeToken']))
-            throw new Exception("The Stripe Token was not generated correctly");
-          Stripe_Charge::create(array("amount" => ($_POST["txtTotalTimbres"]*100),
-                                      "currency" => "hnl",
-                                      "card" => $_POST['stripeToken'],
-                      "description" => $_POST['email']));
-            $htmlDatos["success"]  = '<div class="alert alert-success">
-                      <strong>Success!</strong> Your payment was successful.
-              </div>';
-              actualizarAprobacionEstado($_POST["respuesta"]);
-        }
-        catch (Exception $e) {
-          borrarAprobacion($_POST["respuesta"]);
-          $htmlDatos["error"]  = '<div class="alert alert-danger">
-              <strong>Error!</strong> '.$e->getMessage().'
-              </div>';
-        }
-    }
-    renderizar("realizarPago",array());
+  
+if ($_POST) {
+  Stripe::setApiKey("sk_test_cg3QY2mbcJvek1EVBopLsavG");
+  $htmlDatos["error"] = '';
+  $htmlDatos["success"] = '';
+  try {
+    if (!isset($_POST['stripeToken']))
+      throw new Exception("The Stripe Token was not generated correctly");
+    Stripe_Charge::create(array("amount" => 1000,
+                                "currency" => "usd",
+                                "card" => $_POST['stripeToken']));
+    $htmlDatos["success"] = 'Your payment was successful.';
+  }
+  catch (Exception $e) {
+    $htmlDatos["error"] = $e->getMessage();
+  }
+}
+    renderizar("realizarPago",$htmlDatos);
   }
   run();
 ?>
